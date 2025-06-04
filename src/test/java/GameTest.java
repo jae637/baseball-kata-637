385a5f0bd7c8a94fa6b0114615829fddb02670bf
cjh <kfc@email.com>
2025-06-04 14:54:34 +0900

    [refactor] 58

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions.*;
-
 class GameTest {
 
     private Game game;
@@ -40,24 +38,37 @@ class GameTest {
 
     @Test
     void returnSomething(){
-        game.question = "123";
+        setGameQuestion("123");
         GuessResult result = game.guess("123");
 
-        assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(true);
-        assertThat(result.getStrikes()).isEqualTo(3);
-        assertThat(result.getBalls()).isEqualTo(0);
+        boolean solved = true;
+        int strikes = 3;
+        int balls = 0;
+
+        assertCorrectAnswer(result, solved, strikes, balls);
     }
 
     @Test
     void notMatchedAllNumber(){
-        game.question = "123";
+        setGameQuestion("123");
         GuessResult result = game.guess("456");
 
+        boolean solved = false;
+        int strikes = 0;
+        int balls = 0;
+
+        assertCorrectAnswer(result, solved, strikes, balls);
+    }
+
+    private void setGameQuestion(String number) {
+        game.question = number;
+    }
+
+    private void assertCorrectAnswer(GuessResult result, boolean solved, int strikes, int balls) {
         assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(false);
-        assertThat(result.getStrikes()).isEqualTo(0);
-        assertThat(result.getBalls()).isEqualTo(0);
+        assertThat(result.isSolved()).isEqualTo(solved);
+        assertThat(result.getStrikes()).isEqualTo(strikes);
+        assertThat(result.getBalls()).isEqualTo(balls);
     }
 
 }
